95788c6a71783c844b061dafe35e342458ffd95c
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2022-05-18 19:27:42 +0700

    feat: first init + add navbar and meta tags

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            colors: {
+                black: "#171717",
+                white: "#FDFDFD",
+                primary: {
+                    400: "#D3E7F1",
+                    500: "#3AA8DC",
+                },
+                secondary: {
+                    400: "#895CD0",
+                    500: "#E9DAFF",
+                },
+            },
+        },
+    },
+    plugins: [],
+};
